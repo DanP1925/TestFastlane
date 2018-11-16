7e7f8f1de1340bd126393b2a34ea32d3ae0f906a
Unknown <a20110645@pucp.pe>
2018-11-16 15:15:54 -0500

    Because I'm easy come, easy go

--- app/src/androidTest/java/com/athelas/pruebafastlane/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/athelas/pruebafastlane/ExampleInstrumentedTest.kt
@@ -6,7 +6,6 @@ import android.support.test.espresso.matcher.ViewMatchers.withId
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.athelas.pruebafastlane.MainActivity
-import com.athelas.daniel.pruebafastlane.R
 
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/main/java/com/athelas/pruebafastlane/MainActivity.kt
+++ app/src/main/java/com/athelas/pruebafastlane/MainActivity.kt
@@ -4,7 +4,6 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
-import com.athelas.daniel.pruebafastlane.R
 
 class MainActivity : AppCompatActivity()  {
 
